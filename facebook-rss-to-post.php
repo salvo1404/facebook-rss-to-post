b79918c58fdba1750aac5dd831cc74839a53cb22
salvo1404 <salvatorebalzano@hotmail.it>
2015-09-08 21:37:44 +1000

    Added extra file for constants

--- facebook-rss-to-post.php
+++ facebook-rss-to-post.php
@@ -9,51 +9,10 @@ Author URI:        https://github.com/salvo1404
 License:           GPLv2 or later
 */
 
-use Admin\FacebookRssToPost;
-
-/**
- * Define constants used by the plugin.
- */
-
-if (!defined('FB_RSS_PATH')) {
-    define('FB_RSS_PATH', trailingslashit(plugin_dir_path(__FILE__)));
-}
-
-if (!defined('FB_RSS_URL')) {
-    define('FB_RSS_URL', trailingslashit(plugin_dir_url(__FILE__)));
-}
-
-if (!defined('FB_RSS_BASENAME')) {
-    define('FB_RSS_BASENAME', plugin_basename(__FILE__));
-}
-
-// Set the constant path to the plugin's includes directory.
-if (!defined('FB_RSS_INC')) {
-    define('FB_RSS_INC', FB_RSS_PATH . trailingslashit('includes'), true);
-}
-
-if (!defined('FB_RSS_VERSION')) {
-    define('FB_RSS_VERSION', '1.0.0');
-}
-
-// Facebook API constants
-if (!defined('FB_RSS_API_TOKEN')) {
-    define('FB_RSS_API_TOKEN', 'Bearer 640010092808045|o-nVSrr-QV02pWtJjhHxdli4r00', true);
-}
-if (!defined('FB_RSS_API_URL')) {
-    define('FB_RSS_API_URL', 'https://graph.facebook.com', true);
-}
-if (!defined('FB_RSS_API_VERSION')) {
-    define('FB_RSS_API_VERSION', 'v2.4', true);
-}
-if (!defined('FB_RSS_API_FIELDS')) {
-    define('FB_RSS_API_FIELDS', 'id,name,message,picture,type,link', true);
-}
-if (!defined('FB_RSS_API_MAX_POSTS_DEFAULT')) {
-    define('FB_RSS_API_MAX_POSTS_DEFAULT', '10', true);
-}
+use admin\FacebookRssToPost;
 
 // Includes
+include_once('constants.php');
 include_once('admin/FacebookRssToPost.php');
 include_once(FB_RSS_INC . '/Controllers/FormController.php');
 include_once(FB_RSS_INC . '/Repositories/PostRepositoryInterface.php');
